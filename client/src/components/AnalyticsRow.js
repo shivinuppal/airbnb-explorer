04f53ac00382548c39ee74a38cfbafe36551b71b
Mirriam Chemutai Ronoh <dsri@seas.upenn.edu>
2020-12-16 09:23:00 -0500

    Analytics Page

---
+++ client/src/components/AnalyticsRow.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default class AnalyticsRow extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	/* ---- Q1b (Dashboard) ---- */
+	/* Change the contents (NOT THE STRUCTURE) of the HTML elements to show a movie row. */
+	render() {
+		return (
+			<div className="zip">
+				<div className="zipcode">{this.props.zipcode}</div>
+				<div className="avgprice">{this.props.avgprice}</div>
+			</div>
+		);
+	}
+}
